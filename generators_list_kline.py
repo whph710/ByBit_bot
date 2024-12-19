13355c85f53193dbb83596f188bf4008dfa2fc04
LordKharlanov <maxim10741@gmail.com>
2024-12-20 03:22:49 +0500

    add 2 functions with AI response - dict

--- generators_list_kline.py
+++ generators_list_kline.py
@@ -3,6 +3,6 @@ from request_bybit import get_bybit_last_kline_data
 
 def analyze_timeframes(ticker):
     tf = 5
-    candles = 96
+    candles = 50
     data = get_bybit_last_kline_data(ticker, str(tf), candles)
     return data
--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-import asyncio
 from request_bybit import get_bybit_linear_tickers_usdt
 from generators_list_kline import analyze_timeframes
-from gpt_analyze import  gemini_analyze, gpt_text
-from func import calculate_moving_averages, calculate_rsi, calculate_adx,calculate_stochastic_oscillator, promt
+from func import gpt_analyze
 
 
-async def main():
+def main():
     try:
         for ticket in get_bybit_linear_tickers_usdt():
             data = analyze_timeframes(ticket)
+            a = gpt_analyze(data)
             print(ticket)
-            a = (str(promt([calculate_moving_averages(data),
-                   calculate_rsi(data),
-                   calculate_adx(data),
-                   calculate_stochastic_oscillator(data)])))
-            with open('result.txt', 'a', encoding='utf-8') as file:
-                file.write(ticket + a + '\n')
-            # print(await gpt_text(str(promt([calculate_moving_averages(data),
-            #        calculate_rsi(data),
-            #        calculate_adx(data),
-            #        calculate_stochastic_oscillator(data)]))))
+            print(a)
 
     except Exception as e:
         print(e)
 
+
 # Запуск асинхронной функции main
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
 
